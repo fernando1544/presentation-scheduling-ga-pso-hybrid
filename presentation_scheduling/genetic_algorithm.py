988cc7a400fbe4696856904c4eec2f5472c9cccc
Fernando G. Albornoz <fgalbornoz@proton.me>
2024-08-10 17:23:09 -0300

    feat: comentarios in-code in-line

--- presentation_scheduling/genetic_algorithm.py
+++ presentation_scheduling/genetic_algorithm.py
@@ -1,74 +1,75 @@
 from penalty_function import penalty
 import numpy as np
 
-
-# generate initial population where all hard constraints have been solved except HC02
+# Genera una población inicial donde todas las restricciones duras han sido resueltas, excepto HC02
 def generate_chromosome(slot_presentation):
-    chromosome = np.copy(slot_presentation)
-    slot_no = chromosome.shape[0]
-    presentation_no = chromosome.shape[1]
+    chromosome = np.copy(slot_presentation)  # Crea una copia de la presentación de los slots
+    slot_no = chromosome.shape[0]  # Número total de slots
+    presentation_no = chromosome.shape[1]  # Número total de presentaciones
 
+    # Recorre cada presentación
     for presentation in range(presentation_no):
         while True:
-            random_slot = np.random.randint(slot_no)
-            # if the slot is available and empty
+            random_slot = np.random.randint(slot_no)  # Selecciona un slot aleatorio
+            # Si el slot está disponible y vacío
             if chromosome[random_slot][presentation] == 0 and np.count_nonzero(chromosome[random_slot] == 1) == 0:
-                chromosome[random_slot][presentation] = 1
+                chromosome[random_slot][presentation] = 1  # Asigna la presentación al slot
                 break
 
-    return chromosome
+    return chromosome  # Devuelve el cromosoma generado
 
-
-# select 2 chromosomes based on tournament selection
+# Selecciona 2 cromosomas basándose en la selección por torneo
 def selection(population, penalty_points):
-    tournament_size = 2
+    tournament_size = 2  # Tamaño del torneo
 
-    # select 1st chromosome based on 1st tournament selection
+    # Selecciona el primer cromosoma basado en la primera selección por torneo
     t1, t2 = np.random.choice(range(population.shape[0]), tournament_size)
     first = t1 if penalty_points[t1] <= penalty_points[t2] else t2
 
-    # ensure 2 chromosomes selected are not identical
+    # Asegura que los 2 cromosomas seleccionados no sean idénticos
     while True:
-        # select 2nd chromosome based on 2nd tournament selection
+        # Selecciona el segundo cromosoma basado en la segunda selección por torneo
         t1, t2 = np.random.choice(range(population.shape[0]), tournament_size)
         second = t1 if penalty_points[t1] <= penalty_points[t2] else t2
 
         if second != first:
             break
 
-    return population[first], population[second]
+    return population[first], population[second]  # Devuelve los dos cromosomas seleccionados
 
-
-# perform 2-point crossover
+# Realiza un cruce de 2 puntos
 def crossover(first_parent, second_parent):
-    first_child = np.copy(first_parent)
-    second_child = np.copy(second_parent)
-    presentation_no = first_parent.shape[1]
-    cutpoint1, cutpoint2 = np.random.choice(range(presentation_no), 2)
+    first_child = np.copy(first_parent)  # Crea una copia del primer padre
+    second_child = np.copy(second_parent)  # Crea una copia del segundo padre
+    presentation_no = first_parent.shape[1]  # Número total de presentaciones
+    cutpoint1, cutpoint2 = np.random.choice(range(presentation_no), 2)  # Selecciona dos puntos de corte aleatorios
 
     if cutpoint1 > cutpoint2:
-        cutpoint1, cutpoint2 = cutpoint2, cutpoint1
+        cutpoint1, cutpoint2 = cutpoint2, cutpoint1  # Asegura que cutpoint1 sea menor que cutpoint2
 
-    # swap presentations from cutpoint1 to cutpoint2 between 2 parents
+    # Intercambia las presentaciones entre los dos padres desde cutpoint1 hasta cutpoint2
     first_child[:, cutpoint1:cutpoint2], second_child[:, cutpoint1:cutpoint2] = \
         second_child[:, cutpoint1:cutpoint2], np.copy(first_child[:, cutpoint1:cutpoint2])
+    
+    # Repara los hijos para corregir posibles violaciones de restricciones
     first_child = repair(first_child, cutpoint1, cutpoint2)
     second_child = repair(second_child, cutpoint1, cutpoint2)
-    return first_child, second_child
+    
+    return first_child, second_child  # Devuelve los dos hijos resultantes del cruce
 
-
-# repair chromosome after crossover
+# Repara el cromosoma después del cruce
 def repair(chromosome, cutpoint1, cutpoint2):
-    slot_no = chromosome.shape[0]
+    slot_no = chromosome.shape[0]  # Número total de slots
 
+    # Recorre cada presentación entre los puntos de corte
     for presentation in range(cutpoint1, cutpoint2):
-        slot = np.where(chromosome[:, presentation] == 1)[0][0]
+        slot = np.where(chromosome[:, presentation] == 1)[0][0]  # Encuentra el slot donde está la presentación
 
-        # more than 1 presentation scheduled for a slot
+        # Si hay más de una presentación programada para un slot
         if np.count_nonzero(chromosome[slot] == 1) > 1:
-            chromosome[slot][presentation] = 0
+            chromosome[slot][presentation] = 0  # Desasigna la presentación del slot
 
-            # schedule presentation for another random slot
+            # Asigna la presentación a otro slot aleatorio
             while True:
                 random_slot = np.random.randint(slot_no)
 
@@ -76,62 +77,66 @@ def repair(chromosome, cutpoint1, cutpoint2):
                     chromosome[random_slot][presentation] = 1
                     break
 
-    return chromosome
+    return chromosome  # Devuelve el cromosoma reparado
 
-
-# swap mutation of chromosome after crossover
+# Realiza una mutación por intercambio en el cromosoma después del cruce
 def mutation(chromosome):
-    presentation_no = chromosome.shape[1]
-    random_presentation1 = np.random.randint(presentation_no)
-    slot1 = np.where(chromosome[:, random_presentation1] == 1)[0][0]
+    presentation_no = chromosome.shape[1]  # Número total de presentaciones
+    random_presentation1 = np.random.randint(presentation_no)  # Selecciona una presentación aleatoria
+    slot1 = np.where(chromosome[:, random_presentation1] == 1)[0][0]  # Encuentra el slot para la presentación1
 
     while True:
-        random_presentation2 = np.random.randint(presentation_no)
-        slot2 = np.where(chromosome[:, random_presentation2] == 1)[0][0]
+        random_presentation2 = np.random.randint(presentation_no)  # Selecciona otra presentación aleatoria
+        slot2 = np.where(chromosome[:, random_presentation2] == 1)[0][0]  # Encuentra el slot para la presentación2
 
-        # 2 presentations can be scheduled on slots to be exchanged, hence swap 2 presentations
+        # Intercambia las dos presentaciones entre los dos slots si son intercambiables
         if chromosome[slot1][random_presentation2] == 0 and chromosome[slot2][random_presentation1] == 0:
             chromosome[slot1][random_presentation1] = chromosome[slot2][random_presentation2] = 0
             chromosome[slot1][random_presentation2] = chromosome[slot2][random_presentation1] = 1
             break
 
-    return chromosome
+    return chromosome  # Devuelve el cromosoma mutado
 
-
-# Steady-State Genetic Algorithm - replace 2 chromosomes in population
+# Algoritmo Genético de Estado Estable - reemplaza 2 cromosomas en la población
 def replacement(population, penalty_points, first_child, second_child, first_penalty_point, second_penalty_point):
-    # replace 2 chromosomes of highest penalty points with 2 new chromosomes
+    # Reemplaza los 2 cromosomas de mayor penalización con los 2 nuevos cromosomas
     population_size = len(population)
     population[population_size - 1], population[population_size - 2] = first_child, second_child
     penalty_points[population_size - 1], penalty_points[population_size - 2] = first_penalty_point, second_penalty_point
 
-    # sort population based on penalty points
+    # Ordena la población según los puntos de penalización
     population = population[penalty_points.argsort()]
     penalty_points = penalty_points[penalty_points.argsort()]
 
-    return population, penalty_points
+    return population, penalty_points  # Devuelve la población y los puntos de penalización ordenados
 
-
-# reproduce new chromosomes in new generation
+# Reproduce nuevos cromosomas en la nueva generación
 def reproduction(max_generations, population, penalty_points, presentation_presentation,
                  presentation_supervisor, supervisor_preference):
-    plot_data = []
+    plot_data = []  # Lista para almacenar los puntos de penalización por iteración
 
+    # Ejecuta el ciclo de generaciones
     for generation in range(max_generations):
+        # Selecciona 2 padres
         first_parent, second_parent = selection(population, penalty_points)
+        # Realiza el cruce para generar 2 hijos
         first_child, second_child = crossover(first_parent, second_parent)
+        # Aplica mutación a los hijos
         first_child = mutation(first_child)
         second_child = mutation(second_child)
+        # Calcula los puntos de penalización para los hijos
         first_penalty_point = \
             penalty(first_child, presentation_presentation, presentation_supervisor, supervisor_preference)[0]
         second_penalty_point = \
             penalty(second_child, presentation_presentation, presentation_supervisor, supervisor_preference)[0]
+        # Reemplaza los cromosomas de mayor penalización con los hijos nuevos
         population, penalty_points = \
             replacement(population, penalty_points, first_child, second_child,
                         first_penalty_point, second_penalty_point)
-        plot_data.append(penalty_points[0])
+        plot_data.append(penalty_points[0])  # Almacena el punto de penalización del mejor cromosoma
 
+        # Imprime el punto de penalización cada 50 iteraciones
         if (generation + 1) % 50 == 0:
             print("[Iteration ", generation + 1, "] Penalty Point: ", penalty_points[0], sep="")
 
-    return population, penalty_points, plot_data
+    return population, penalty_points, plot_data  # Devuelve la población, los puntos de penalización y los datos de gráficos
--- presentation_scheduling/penalty_function.py
+++ presentation_scheduling/penalty_function.py
@@ -1,28 +1,29 @@
 import numpy as np
 from numba import njit
 
-
-# calculate penalty points based on hard and soft constraints
-@njit(cache=True)  # decorate function to be compiled instead of interpreted, speed up code execution by 2-3 times
+# Calcula los puntos de penalización basados en restricciones duras y suaves
+@njit(cache=True)  # Decora la función para ser compilada en lugar de interpretada, acelerando la ejecución del código de 2 a 3 veces
 def penalty(slot_presentation, presentation_presentation, presentation_supervisor, supervisor_preference):
-    penalty_point = 0
-    hc_count = 0
-    sc_count = 0
-    presentation_no = slot_presentation.shape[1]
-    supervisor_no = supervisor_preference.shape[0]
-    venue_no = 4
-    time_slot_no = 15
-    day_slot_no = venue_no * time_slot_no
-    day_no = 5
-    presentation_presentation = np.copy(presentation_presentation)
-    supervisor_preference[:, 3:6].fill(0)
+    penalty_point = 0  # Inicializa el punto de penalización total
+    hc_count = 0  # Contador de violaciones de restricciones duras
+    sc_count = 0  # Contador de violaciones de restricciones suaves
+    presentation_no = slot_presentation.shape[1]  # Número total de presentaciones
+    supervisor_no = supervisor_preference.shape[0]  # Número total de supervisores
+    venue_no = 4  # Número total de lugares
+    time_slot_no = 15  # Número total de intervalos de tiempo
+    day_slot_no = venue_no * time_slot_no  # Número total de intervalos de tiempo por día
+    day_no = 5  # Número total de días
+    presentation_presentation = np.copy(presentation_presentation)  # Copia para manipulación segura
+    supervisor_preference[:, 3:6].fill(0)  # Inicializa las preferencias de los supervisores (columnas 3 a 5)
 
-    # HC02: no staff can attend more than 1 presentations concurrently
+    # HC02: ningún personal puede asistir a más de 1 presentación simultáneamente
     for presentation in range(presentation_no):
+        # Encuentra el slot de la presentación actual
         slot = np.where(slot_presentation[:, presentation] == 1)[0][0]
         min_concurrent_slot = (slot % time_slot_no) + (slot // day_slot_no) * day_slot_no
         max_concurrent_slot = min_concurrent_slot + day_slot_no
 
+        # Verifica los slots concurrentes
         for concurrent_slot in range(min_concurrent_slot, max_concurrent_slot, time_slot_no):
             concurrent_presentation = np.where(slot_presentation[concurrent_slot] == 1)[0]
 
@@ -32,27 +33,30 @@ def penalty(slot_presentation, presentation_presentation, presentation_superviso
                 if presentation_presentation[presentation][concurrent_presentation] == 1:
                     presentation_presentation[presentation][concurrent_presentation] = -1
                     presentation_presentation[concurrent_presentation][presentation] = -1
-                    penalty_point += 1000
+                    penalty_point += 1000  # Penalización alta por restricción dura
                     hc_count += 1
 
-    # 5(day) × 15(time slot) matrix storing venue for each presentation
-    day_time_slot = np.zeros((day_no, time_slot_no + 1), dtype=np.int8)  # extra last column to handle last time slot
+    # Matriz 5x15 que almacena el lugar para cada presentación
+    day_time_slot = np.zeros((day_no, time_slot_no + 1), dtype=np.int8)  # Columna extra para manejar el último intervalo
 
-    for supervisor in range(supervisor_no):  # most time-consuming loop
+    # Verifica las preferencias de los supervisores
+    for supervisor in range(supervisor_no):  # Bucle más costoso en tiempo
         supervised_presentations = np.where(presentation_supervisor[:, supervisor] == 1)[0]
         day_time_slot.fill(0)
 
+        # Rellena la matriz con las presentaciones supervisadas
         for supervised_presentation in supervised_presentations:
             supervised_slot = np.where(slot_presentation[:, supervised_presentation] == 1)[0][0]
             supervised_day = supervised_slot // day_slot_no
             supervised_time_slot = supervised_slot % time_slot_no
-            supervised_venue = (supervised_slot // time_slot_no) % venue_no + 1  # add 1 to avoid conflict with 0
+            supervised_venue = (supervised_slot // time_slot_no) % venue_no + 1  # Añade 1 para evitar conflicto con 0
             day_time_slot[supervised_day][supervised_time_slot] = supervised_venue
 
-        consecutive_preference = supervisor_preference[supervisor][0]  # SC01: consecutive presentations
+        consecutive_preference = supervisor_preference[supervisor][0]  # SC01: presentaciones consecutivas
         day_count = 0
         venue_changes = 0
 
+        # Recorre los días y los intervalos de tiempo para evaluar las preferencias
         for day in range(day_no):
             is_consecutive = False
             is_this_day = False
@@ -63,8 +67,7 @@ def penalty(slot_presentation, presentation_presentation, presentation_superviso
                 if day_time_slot[day][time_slot] != 0:
                     venue = day_time_slot[day][time_slot]
 
-                    # if current presentation is consecutive with previous presentation
-                    # this check is ignored if it is the first presentation of group of consecutive presentations
+                    # Verifica si la presentación actual es consecutiva con la anterior
                     if is_consecutive and venue != previous_venue:
                         venue_changes += 1
 
@@ -73,32 +76,32 @@ def penalty(slot_presentation, presentation_presentation, presentation_superviso
                     consecutive_count += 1
                     previous_venue = venue
                 else:
-                    # calculate penalty points for a group of consecutive presentations (might has only 1 presentation)
+                    # Calcula la penalización para un grupo de presentaciones consecutivas
                     if is_consecutive:
-                        if consecutive_count < consecutive_preference:  # encourage presentations to be consecutive
+                        if consecutive_count < consecutive_preference:  # Fomenta presentaciones consecutivas
                             penalty_point += (consecutive_preference - consecutive_count) * 1
                             supervisor_preference[supervisor][3] += 1
-                        elif consecutive_count > consecutive_preference:  # exceeds maximum consecutive preference
+                        elif consecutive_count > consecutive_preference:  # Excede la preferencia máxima de consecutivas
                             penalty_point += (consecutive_count - consecutive_preference) * 10
                             supervisor_preference[supervisor][3] += 1
                             sc_count += 1
 
                     is_consecutive = False
 
-            if is_this_day:  # a presentation takes place on this day
+            if is_this_day:  # Se lleva a cabo una presentación en este día
                 day_count += 1
 
-        days_preference = supervisor_preference[supervisor][1]  # SC02: number of days
+        days_preference = supervisor_preference[supervisor][1]  # SC02: número de días
         supervisor_preference[supervisor][4] = day_count
 
         if day_count > days_preference:
             penalty_point += (day_count - days_preference) * 10
             sc_count += 1
 
-        venue_preference = supervisor_preference[supervisor][2]  # SC03: change of venue
+        venue_preference = supervisor_preference[supervisor][2]  # SC03: cambio de lugar
         supervisor_preference[supervisor][5] = venue_changes
 
-        if venue_preference == 1 and venue_changes > 0:  # supervisor does not want to change venue
+        if venue_preference == 1 and venue_changes > 0:  # El supervisor no quiere cambiar de lugar
             penalty_point += venue_changes * 10
             sc_count += 1
 
